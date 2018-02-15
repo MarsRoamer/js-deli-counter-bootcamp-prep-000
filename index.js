5c7e40235d117ebf950816559847367c68592c8e
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-15 20:08:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeli = [];
+var KatzDeliLine = [];
+
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katzDeliLine[0] +1} in line.`
+}
