9c8c850c65018fff40019435bfd9a5e045e6266e
Jonathan Tiede <skysoldiermars@gmail.com>
2018-02-15 20:16:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name) + 1} in line.`
 }
+
+function nowServing() {
+  var person = katzDeliLine[0];
+    if(katzDeliLine.length > 0) {}
+    katzDeliLine.splice(0, 1);
+    return person;
+  } else {
+    return 'There is nobody waiting to be served!'
+  }
+}
